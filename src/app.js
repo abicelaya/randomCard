9bbc1cb60fdc14036477dc0c3ae079e455cbf4fe
abicelaya <abicelaya@gmail.com>
2022-04-08 16:29:50 +0000

    random card termianda

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 import "./style.css";
 
-window.onload = function() {};
+window.onload = function() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let randomNumber = Math.floor(Math.random() * 12);
+
+  let randomPalo = palos[randomNumber % 4];
+
+  let spanTop = document.getElementsByClassName("top-palo");
+
+  spanTop[0].innerHTML = randomPalo;
+
+  let spanNumber = document.getElementsByClassName("number");
+
+  spanNumber[0].innerHTML = randomNumber;
+
+  let spanBottom = document.getElementsByClassName("bottom-palo");
+  spanBottom[0].innerHTML = randomPalo;
+};
